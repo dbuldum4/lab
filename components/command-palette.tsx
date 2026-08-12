ce3d22cdf4070598fbd871162711c7e6052eace2
Deniz Aydin Buldum <59258696+dbuldum4@users.noreply.github.com>
2026-08-12 17:53:12 -0400

    Keep import cancellation callback event-free

--- components/command-palette.tsx
+++ components/command-palette.tsx
@@ -622,7 +622,7 @@ export function CommandPalette({
                     type="button"
                     className="feature-button"
                     disabled={importConfirming}
-                    onClick={cancelMarkdownImport}
+                    onClick={() => cancelMarkdownImport()}
                   >
                     Cancel
                   </button>
